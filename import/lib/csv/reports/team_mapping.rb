fae93470b984d75f217693296367fef9f254bf34
Gerald Bauer <gerald.bauer@gmail.com>
2018-07-09 22:00:23 +0200

    up

--- import/lib/csv/reports/team_mapping.rb
+++ import/lib/csv/reports/team_mapping.rb
@@ -15,25 +15,25 @@ def build( team_names )
 
 
   ## show list of teams without known canoncial/pretty print name
-  missing_names = []
+  missing_teams = []
 
   team_names.each do |team_name|
-    missing_names << team_name     if canonical_teams[team_name].nil?
+    missing_teams << team_name     if canonical_teams[team_name].nil?
   end
 
   buf = ''
 
-  if missing_names.size > 0
-    missing_names = missing_names.sort   ## sort from a-z
+  if missing_teams.size > 0
+    missing_teams = missing_teams.sort   ## sort from a-z
 
     buf << "#{missing_teams.size} missing / unknown / (???) teams:\n"
-    buf << "#{missing_names.join(', ')}\n"
+    buf << "#{missing_teams.join(', ')}\n"
     buf << "\n\n"
 
     ############################
     ## for easy update add cut-n-paste code snippet
     buf << "```\n"
-    missing_names.each do |team_name|
+    missing_teams.each do |team_name|
       buf << ("%-22s =>\n" % team_name)
     end
     buf << "```\n\n"
