1205c416186ec5ce76f1ad47ccab4ebb2d7f86be
Gerald Bauer <gerald.bauer@gmail.com>
2018-08-04 21:08:18 +0200

    add csvutils

---
+++ csvutils/lib/csvutils.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+require 'pp'
+require 'csv'
+require 'date'
+require 'fileutils'
+
+
+###
+# our own code
+require 'csvutils/version' # let version always go first
+
+
+
+
+
+puts CsvUtils.banner   # say hello
