15c537f72410db41025fe2478e779edef3d6c86d
Gerald Bauer <gerald.bauer@gmail.com>
2019-06-19 17:50:56 +0200

    Create footballdata_ii.rb

---
+++ footballdata_ii.rb
@@ -0,0 +1,53 @@
+
+## all seasons in-one-file datasets
+FOOTBALLDATA_SOURCES_II =
+{
+    ## https://www.football-data.co.uk/new/<code>.csv
+    ##
+    ##  check - adjust time-zone/time-offset
+    ##
+    ## - austria
+    ## - major-league-soccer
+    ## - mexico
+    ## - brazil
+    ##
+    ##  if no "national" repo, use the "world" repo
+    ##
+    ##  check for seasons format
+    ##     is a full year e.g. 2016 (and NOT 2016/17)
+    ##       e.g. bra,arg, fin, and others!!!!
+
+   ar: 'ARG',  # Argentina Football Results - Primera Division
+   at: 'AUT',  # Austria Football Results   - Bundesliga
+   br: 'BRA',  # Brazil Football Results    - Serie A
+   cn: 'CHN',  # China Football Results     - Super League
+   dk: 'DNK',  # Denmark Football Results  - Superliga
+   fi: 'FIN',  # Finland Football Results  - Veikkausliiga
+   ie: 'IRL',  # Ireland Football Results  - Premier Division
+   jp: 'JPN',  # Japan Football Results    - J-League
+   mx: 'MEX',  # Mexico Football Results   - Liga MX
+   no: 'NOR',  # Norway Football Results  - Eliteserien
+   pl: 'POL',  # Poland Football Results  - Ekstraklasa
+   ro: 'ROU',  # Romania Football Results  - Liga 1
+   ru: 'RUS',  # Russia Football Results  - Premier League
+   se: 'SWE',  # Sweden Football Results  - Allsvenskan
+   ch: 'SWZ',  # Switzerland Football Results - Super League
+   us: 'USA',  # USA Football Results  - MLS
+}
+
+## todo: add (auto-) download script
+
+## note: define tasks for all countries
+FOOTBALLDATA_SOURCES_II.each do |k,basename|
+  country_code    = k
+
+  ## step 1: fetch (download) datasets
+  task "get#{country_code}".to_sym do |t|
+    fetch_repo_ii( country_path, basename )
+  end
+
+  ## step 2: convert datasets
+  task country_code do |t|
+    convert_repo_ii( country_path, basename )
+  end
+end
