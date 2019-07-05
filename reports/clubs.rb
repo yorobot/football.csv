b3a00e3f5927563fc2a56357db742b098b42f599
Gerald Bauer <gerald.bauer@gmail.com>
2019-07-05 20:54:34 +0200

    up

--- reports/clubs.rb
+++ reports/clubs.rb
@@ -43,6 +43,120 @@ alias_method :render, :build
 
 
 end # class TeamMappingsPart
+
+
+class TeamsByCityPart
+
+def initialize( teams )
+  @teams = teams
+end
+
+def build     ## todo/check: always use render as name - why? why not?
+  cities        = {}
+
+  @teams.each do |team|
+    team_city = team.city || '?'    ## convert nil to ?
+    cities[team_city] ||= []
+    cities[team_city] << team
+  end
+
+
+  buf = ''
+
+  ## sort cities by name
+  ##   todo/fix: exlude special key x and ? - why? why not?
+  sorted_cities = cities.to_a.sort do |l,r|
+     res = r[1].size <=> l[1].size       ## sort by team size/counter first
+     res = l[0] <=> r[0]    if res == 0   ## sort by city name next
+     res
+  end
+
+
+  sorted_cities.each do |city_rec|
+    city = city_rec[0]  # city name/key
+    v    = city_rec[1]  # teams for city
+
+      if city == '?'
+        buf << "- #{city}"
+      else
+        buf << "- **#{city}**"
+      end
+
+      buf << " (#{v.size})"
+      buf << ": "
+
+      if v.size == 1
+        t = v[0]
+        buf << "#{t.name} "
+        if t.alt_names && t.alt_names.size > 0
+            ##  todo/fix:
+            ##    add check for matching city name !!!!
+            ##     sort by smallest first - why? why not?
+            buf << " (#{t.alt_names.size}) #{t.alt_names.join(' • ')}"
+        end
+        buf << "\n"
+      else
+        ## buf << v.map { |t| t.name }.join( ', ')  ## print all canonical team names
+        buf << "\n"
+        v.each do |t|
+          buf << "  - #{t.name} "
+          if t.alt_names && t.alt_names.size > 0
+            ##  todo/fix:
+            ##    add check for matching city name !!!!
+            ##     sort by smallest first - why? why not?
+            buf << " (#{t.alt_names.size}) #{t.alt_names.join(' • ')}"
+          end
+          buf << "\n"
+        end
+      end
+  end
+
+  buf << "\n\n"
+  buf
+end  # method build
+
+alias_method :render, :build
+
+end  # class TeamsByCityPart
+
+
+
+##
+##  todo/fix:  pass in path to cut-off root-path from datafile
+##   e.g.  south-america/ar-argentina/clubs.txt
+##            becomes   /ar-argentina/clubs.txt
+class TeamDatafilePart
+
+def initialize( teams_list )
+  @teams_list = teams_list
+end
+
+def build   ## todo/check: always use render as name - why? why not?
+
+  teams_count = @teams_list.reduce(0) { |acc,item| acc+item[1].size }
+
+  buf = ''
+  buf << "#{@teams_list.size} datafiles, #{teams_count} clubs"
+  buf << "\n\n"
+
+  @teams_list.each do |teams_item|
+     teams_datafile = teams_item[0]
+     teams          = teams_item[1]
+
+     buf << "**#{teams_datafile}**"
+     buf << " _(#{teams.size})_"
+
+     team_names = teams.map { |team| team.name }
+     buf << "  #{team_names.join(' • ')}"
+     buf << "\n\n"
+  end
+
+  buf
+end  # method build
+
+alias_method :render, :build
+
+end # class TeamDatafilePart
 end # module TeamIndexer
 
 
@@ -68,7 +182,7 @@ def walk_dir( path, root_path:, level: )
    ## pp entries
 
    files = []
-   team_count = 0
+   teams_list = []
    entries.each do |name|
      entry_path = "#{path}/#{name}"
      ## note: cut-off in_root (to pretty print path)
@@ -78,7 +192,7 @@ def walk_dir( path, root_path:, level: )
        next  if ['.', '..', '.git'].include?( name )
 
        puts "   #{level} walking #{entry_path_rel}... "
-       team_count += walk_dir( entry_path, root_path: root_path, level: level+1 )
+       teams_list += walk_dir( entry_path, root_path: root_path, level: level+1 )
      else
        if ['clubs.txt'].include?( name )
           files << entry_path_rel
@@ -88,7 +202,7 @@ def walk_dir( path, root_path:, level: )
 
    if files.size > 0
      ## todo/fix: warn if teams > 0
-     if team_count > 0
+     if teams_list.size > 0
         puts "** WARN - do NOT put clubs.txt in non-terminal nodes / sub folders"
         exit 1
      end
@@ -97,18 +211,33 @@ def walk_dir( path, root_path:, level: )
 
      files.each do |file|
        teams = SportDb::Import::TeamReader.read( "#{root_path}/#{file}" )
-       buf = TeamIndexer::TeamMappingsPart.new( teams ).build
-       puts buf
+
+       teams_list << [file, teams]
+
+       buf  = TeamIndexer::TeamMappingsPart.new( teams ).build
+       buf << "\n\n"
+       buf << TeamIndexer::TeamsByCityPart.new( teams ).build
+
 
        report_path = "#{root_path}/#{File.dirname(file)}/README.md"
-       puts "   !! writing report to >#{report_path}<..."
+       puts "   !! writing report (teams) to >#{report_path}<..."
        File.open( report_path, 'w:utf8' ) { |f| f.write buf }
      end
    else
-     puts "   process summary #{team_count} datafiles in level #{level} >#{path}<:"
+     puts "   process summary #{teams_list.size} datafiles in level #{level} >#{path}<:"
+
+     buf = TeamIndexer::TeamDatafilePart.new( teams_list ).build
+
+     report_path =  if level == 1
+                      "#{path}/SUMMARY.md"
+                    else
+                      "#{path}/README.md"
+                    end
+     puts "   !! writing report (datafiles) to >#{report_path}<..."
+     File.open( report_path, 'w:utf8' ) { |f| f.write buf }
    end
 
-   team_count + files.size   ## return number of club/team datafiles
+   teams_list
 end
 
 
