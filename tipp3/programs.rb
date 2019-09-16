23be0c4ede8bfabb32912fcdbdbeef0b5568d2f6
Gerald Bauer <gerald.bauer@gmail.com>
2019-09-16 10:30:58 +0200

    up

--- tipp3/programs.rb
+++ tipp3/programs.rb
@@ -5,8 +5,10 @@ PROGRAMS = %w[
   34a 34b
   35a 35b
   36a 36b
+  37a
 ]
 
+## todo/fix: rename to EXCLUDE_LEAGUES - why? why not?
 NATIONAL_TEAM_LEAGUES = [    # note: skip (ignore) all leagues/cups/tournaments with national (selction) teams for now
   'EM Q',       # Europameisterschaft Qualifikation
   'U21 EMQ',    # U21 EM Qualifikation
@@ -14,6 +16,9 @@ NATIONAL_TEAM_LEAGUES = [    # note: skip (ignore) all leagues/cups/tournaments 
   'INT FS',     # Internationale Freundschaftsspiele
   'FS U21',     # U21 Freundschaftsspiele
   'FS U20',     # U20 Freundschaftsspiele
+  ## national leagues (women)
   'INT FSD',    # Internationale Freundschaftsspiele, Damen
   'EMQDA',      # EM Qualifikation, Damen
+  ## todo/fix: move to clubs leagues (women) - why? why not?
+  'CL DAM',     # UEFA Champions League Damen
 ]
