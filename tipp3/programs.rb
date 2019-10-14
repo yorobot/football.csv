f0e28da347dcd2270fc7b715b04c6dc004df652b
Gerald Bauer <gerald.bauer@gmail.com>
2019-10-14 17:04:33 +0200

    Update programs.rb

--- tipp3/programs.rb
+++ tipp3/programs.rb
@@ -2,11 +2,16 @@
 ## shared list of tipp3 programs
 
 PROGRAMS = %w[
-  34a 34b
-  35a 35b
-  36a 36b
-  37a
+  34a 34b       ## Tu-Th Aug/20-22 +++ Fr-Mo Aug/23-26
+  35a 35b       ## Tu-Th Aug/27-29 +++ Fr-Mo Aug/30-Sep/2
+  36a 36b       ## Tu-Th Sep/3-5   +++ Fr-Mo Sep/6-9
+  37a 37b       ## Tu-Th Sep/10-12 +++ Fr-Mo Sep/13-16
+  38a 38b       ## Tu-Th Sep/17-19 +++ Fr-Mo Sep/20-23
+  39a 39b       ## Tu-Th Sep/24-26 +++ Fr-Mo Sep/27-30
+  40a 40b       ## Tu-Th Oct/1-3   +++ Fr-Mo Oct/4-7
+  41a           ## Tu-Th Oct/8-10   
 ]
+
 
 ## todo/fix: rename to EXCLUDE_LEAGUES - why? why not?
 NATIONAL_TEAM_LEAGUES = [    # note: skip (ignore) all leagues/cups/tournaments with national (selction) teams for now
