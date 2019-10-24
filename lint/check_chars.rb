01ccf8cb8d1861d1587777f29d91b4a5d6499b0f
Gerald Bauer <gerald.bauer@gmail.com>
2019-10-24 15:06:42 +0200

    Create check_chars.rb

---
+++ lint/check_chars.rb
@@ -0,0 +1,54 @@
+require 'pp'
+
+##
+#  check for tabs (\t) and warn
+#  check for unicode spaces (e.g. \u2002) and warn
+#  check for unicode minus sign (e.g. \u2212)) and warn
+
+class CharLinter
+
+  def self.read( path )   ## use - rename to read_file or from_file etc. - why? why not?
+    txt = File.open( path, 'r:utf-8' ).read
+    puts "reading >#{path}<..."
+    parse( txt )
+  end
+
+  def self.fix( path )   ## todo/check: rename to read_fix or update or fixup
+    txt_fixed = read( path )
+    File.open( path, 'w:utf-8' ) { |f| f.write txt_fixed }
+    txt_fixed
+  end
+
+
+  def self.parse( txt )
+    txt_fixed = String.new
+    lineno = 0
+    txt.each_line do |line|    # note: line keeps (includes) trailing newline/line feed (\n or \r\n)
+      lineno += 1
+
+      line_fixed = line.gsub( "\u{2002}" ) do |_|
+        puts "** !!!WARN!!! line #{lineno}: replacing unicode space (U+2002) >\u{2002}< in:"
+        puts "              >>#{line}"
+        " "  ## use regular ascii space
+      end
+      line_fixed = line_fixed.gsub( "\u{2212}" ) do |_|
+        puts "** !!!WARN!!! line #{lineno}: replacing unicode minus sign (U+2212) >\u{2212}< in:"
+        puts "              >>#{line}"
+        "-"  ## use regular ascii minus ("dash")
+      end
+      line_fixed = line_fixed.gsub( "\t" ) do |_|
+        puts "** !!!WARN!!! line #{lineno}: replacing tab (\\t) >\t< in:"
+        puts "              >>#{line}"
+        " "  ## use regular ascii space
+      end
+
+      txt_fixed << line_fixed
+    end
+
+    txt_fixed
+  end
+end   # class CharLinter
+
+
+
+##  CharLinter.fix( "2012-13.conf.txt" )
