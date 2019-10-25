c68a2c1fb5a79338ac5498365f6444840e910af2
Gerald Bauer <gerald.bauer@gmail.com>
2019-10-25 12:11:12 +0200

    up

--- clubs/check_clubs_conf.rb
+++ clubs/check_clubs_conf.rb
@@ -66,13 +66,16 @@ CONF_PATTERN = %r{
                   /\.conf\.txt$
                  }x
 
-datafiles = find_datafiles( '../../../openfootball/austria', CONF_PATTERN )
+# datafiles = find_datafiles( '../../../openfootball/austria', CONF_PATTERN )
+# datafiles = find_datafiles( '../../../openfootball/deutschland', CONF_PATTERN )
+datafiles = find_datafiles( '../../../openfootball/england', CONF_PATTERN )
 pp datafiles
 
 datafiles.each do |datafile|
 
-   ## CharLinter.read( datafile )    ## check for non-ascii chars, tabs, etc.
-   ## CharLinter.fix( datafile )
+   # CharLinter.read( datafile )    ## check for non-ascii chars, tabs, etc.
+   # CharLinter.fix( datafile )
+   # next
 
 
   events = ConfClubLintReader.read( datafile )
