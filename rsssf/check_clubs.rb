9abe8b25fc01a5d7378b14eb563c17243950a3cd
Gerald Bauer <gerald.bauer@gmail.com>
2019-11-17 17:40:33 +0100

    up rsssf

--- rsssf/check_clubs.rb
+++ rsssf/check_clubs.rb
@@ -3,35 +3,35 @@ require_relative '../lint/check_clubs'
 
 
 
-RSSSF_PATTERN = %r{
+RSSSF_RE = %r{
                   /[a-z]{2,3}.txt$
                  }x
 
 
 File.open( 'missing_clubs.txt', 'w:utf-8' ) do |out|
 
-datafiles = find_datafiles( '2018-19', RSSSF_PATTERN )
+datafiles = Datafile.find( '2018-19', RSSSF_RE )
 datafiles.each do |datafile|
 
-  countries = ClubLintReader.read( datafile )
-  pp countries
+  nodes = SportDb::Import::ClubLintReader.read( datafile )
+  pp nodes
 
-  missing_clubs = check_clubs_by_countries( countries )
-  pp missing_clubs
+  count = check_clubs_by_countries( nodes )
+  pp count
 
-  if missing_clubs[0][1].empty?
+  if count == 0
     puts "** OK"
   else
-    puts "** !!! ERROR !!! club names missing"
+    puts "** !!! ERROR !!! #{count} club names missing"
     ## exit 1
 
-    out.puts
-    out.puts "=========="
-    out.puts missing_clubs[0][0]
-    out.puts
-    missing_clubs[0][1].each do |name|
-      out.puts name
-    end
+    # out.puts
+    # out.puts "=========="
+    # out.puts missing_clubs[0][0]
+    # out.puts
+    # missing_clubs[0][1].each do |name|
+    #  out.puts name
+    # end
   end
 end
 end  ## File.open
